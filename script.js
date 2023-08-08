5a2c154dcd662e8d0569037f8cce6028b2fefa1b
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-08-08 09:22:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click' , toMakeDelay);
+async function toMakeDelay(){
+	const text = document.getElementById('text').value
+	const delay = document.getElementById('delay').value
+	const output = document.getElementById('output');
+	// output.innerText = '';
+	await new Promise(resolve => setTimeout(resolve , delay) ) 
+	output.innerText = text;
+	
+}
